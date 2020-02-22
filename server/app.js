149ec980929458ed9b81578893b41f3776601ba3
Joao Santos <joaomsglds@gmail.com>
2020-02-22 23:27:14 +0100

    use cors middleware in express to allow client req

--- server/app.js
+++ server/app.js
@@ -2,10 +2,13 @@ const bookmarkRouter = require("./routes/bookmarkRoutes");
 const repoRouter = require("./routes/repoRoutes");
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 // middleware for parsing the body in req
 app.use(express.json());
+// allow calling the API from the Frontend
+app.use(cors());
 
 // ROUTES
 app.use("/api/v1/repos", repoRouter);
