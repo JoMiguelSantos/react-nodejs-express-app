92e7d881e3987aa8d3651b62af54052bf1f63b1d
Joao Santos <joaomsglds@gmail.com>
2020-03-05 10:34:37 +0100

    clear DB before testing empty states

--- client/cypress/integration/routes.spec.js
+++ client/cypress/integration/routes.spec.js
@@ -1,4 +1,18 @@
 describe("routes", () => {
+  before(() => {
+    cy.request("http://localhost:4000/api/v1/bookmarks").then(res =>
+      res.body.data.data.forEach(repo => {
+        cy.request({
+          url: "http://localhost:4000/api/v1/bookmarks",
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({ repoId: repo.id })
+        });
+      })
+    );
+  });
   it("renders all pages empty state", () => {
     cy.visit("/");
     cy.location().should(location => {
