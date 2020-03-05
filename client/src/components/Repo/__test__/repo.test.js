34e0fb5f3530f576f5749bfb4ef31d08df322e51
Joao Santos <joaomsglds@gmail.com>
2020-03-05 10:34:06 +0100

    update test for Repo according to previous change

--- client/src/components/Repo/__test__/repo.test.js
+++ client/src/components/Repo/__test__/repo.test.js
@@ -1,17 +1,15 @@
 import Repo from "../Repo";
 import React from "react";
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import { render, waitForElement, fireEvent } from "@testing-library/react";
-import { shallow, mount } from "enzyme";
 import reducer, { initialState } from "../../../store/reducers/bookmarks";
 import { FetchMock } from "@react-mock/fetch";
 
 // need to add composeReducers if want to test reducers/actions
-function renderWithReduxFetchState({
+function renderWithReduxFetch({
   initialState,
-  isRepoBookmarked,
-  store = createStore(reducer, initialState)
+  store = createStore(combineReducers({ bookmarks: reducer }), initialState)
 } = {}) {
   return {
     ...render(
@@ -30,13 +28,14 @@ function renderWithReduxFetchState({
         ]}
       >
         <Provider store={store}>
-          <Repo isRepoBookmarked={isRepoBookmarked} />
+          <Repo />
         </Provider>
       </FetchMock>
     ),
     store
   };
 }
+
 // test renders correct Repo item given data example
 const mockRepo = {
   id: 183743694,
@@ -49,41 +48,28 @@ const mockRepo = {
   watchers_count: 3563,
   language: "javascript",
   open_issues_count: 124,
-  forks: 1355
+  forks_count: 1355
 };
 
 describe("<Repo /> unit tests", () => {
   // SNAPSHOT
   it("matches the previous snapshot", () => {
-    const { container } = renderWithReduxFetchState();
+    const { container } = renderWithReduxFetch();
     expect(container).toMatchSnapshot();
   });
 
   it("renders initial state with Bookmark text visible", async () => {
-    // Render new instance in every test to prevent leaking state
-    const { getByText } = renderWithReduxFetchState({
-      isRepoBookmarked: false
-    });
+    const { getByText } = renderWithReduxFetch();
     await waitForElement(() => getByText("Bookmark"));
   });
 
-  it("renders bookmarked state with Unbookmark text visible", async () => {
-    // Render new instance in every test to prevent leaking state
-    const { getByText } = renderWithReduxFetchState({ isRepoBookmarked: true });
-    await waitForElement(() => getByText("Unbookmark"));
-  });
+  it("toggles un/bookmark", async () => {
+    const { getByText } = renderWithReduxFetch();
+    await waitForElement(() => getByText("Bookmark"));
 
-  it("toggles Bookmark button to Unbookmark visible when clicked", async () => {
-    const { getByText } = renderWithReduxFetchState({
-      isRepoBookmarked: false
-    });
     fireEvent.click(getByText("Bookmark"));
     await waitForElement(() => getByText("Unbookmark"));
-  });
 
-  it("toggles Unbookmark button back to Bookmark visible when clicked", async () => {
-    // Render new instance in every test to prevent leaking state
-    const { getByText } = renderWithReduxFetchState({ isRepoBookmarked: true });
     fireEvent.click(getByText("Unbookmark"));
     await waitForElement(() => getByText("Bookmark"));
   });
